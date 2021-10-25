66f7552cafed23125bb27f19904f7903af876ec0
Héctor Vera <juniomilnovecientos@gmail.com>
2021-10-25 00:45:47 -0300

    Add skeleton to memory mapped register handlers.
    NTSC2C02 class:
    - Add skeleton to handle memory mapped register read and writes.

--- NES/NTSC2C02/memory.cpp
+++ NES/NTSC2C02/memory.cpp
@@ -13,9 +13,80 @@ void NTSC2C02::writePPUMemory(uint16_t address, uint8_t value) {
 }
 
 uint8_t NTSC2C02::readCPUMemory(uint16_t address) const {
+    if (address >= 0x2000 && address < 0x4000) {
+        switch (address % 7) {
+            case 0: {
+
+                break;
+            }
+            case 1: {
+
+                break;
+            }
+            case 2: {
+
+                break;
+            }
+            case 3: {
+
+                break;
+            }
+            case 4: {
+
+                break;
+            }
+            case 5: {
+
+                break;
+            }
+            case 6: {
+
+                break;
+            }
+            case 7: {
+
+                break;
+            }
+        }
+    }
+
     return bus->readCPUMemory(address);
 }
 
 void NTSC2C02::writeCPUMemory(uint16_t address, uint8_t value) {
+    if (address >= 0x2000 && address < 0x4000) {
+        switch (address % 7) {
+            case 0: {
+                PPUCTRL = value;
+                break;
+            }
+            case 1: {
+                PPUMASK = value;
+                break;
+            }
+            case 2: {
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+            case 5: {
+
+                break;
+            }
+            case 6: {
+
+                break;
+            }
+            case 7: {
+
+                break;
+            }
+        }
+    }
+
     bus->writeCPUMemory(address, value);
 }
