817a80bd4e1fc3ad67593151cd2420fa31bc76c4
Héctor Vera <juniomilnovecientos@gmail.com>
2021-11-19 18:08:22 -0300

    Move Canvas implementation to .cpp file, add small input processing to close the window (this shouldn't be here but I want to fix other things first).
    Canvas class:
    - Moved implementation of methods to .cpp file.
    - Add processInput method to close the window.
    
    CMakeLists:
    - Add verbose flag (need to fix SDL2 include, it's currently using an absolute path).

--- Platform/Canvas/Canvas.cpp
+++ Platform/Canvas/Canvas.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "Canvas.h"
+
+Canvas::Canvas(std::string const& title, int width, int height, int x, int y) : event() {
+    window = SDL_CreateWindow(title.c_str(), x, y, width, height, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+    frameBuffer = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, width, height);
+    pitch = width * static_cast<int>(sizeof(uint32_t));
+}
+
+Canvas::~Canvas() {
+    SDL_DestroyTexture(frameBuffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+}
+
+void Canvas::drawFrame(uint32_t* pixels) {
+    SDL_UpdateTexture(frameBuffer, nullptr, pixels, pitch);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, frameBuffer, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
+}
+
+bool Canvas::processInput() {
+    bool quit = false;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                quit = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    return quit;
+}
--- Platform/Canvas/Canvas.h
+++ Platform/Canvas/Canvas.h
@@ -9,33 +9,19 @@
 
 class Canvas {
 public:
-    Canvas(std::string const& title, int width, int height, int x = SDL_WINDOWPOS_CENTERED, int y = SDL_WINDOWPOS_CENTERED) {
-        window = SDL_CreateWindow(title.c_str(), x, y, width, height, SDL_WINDOW_SHOWN);
-        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
-        frameBuffer = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, width, height);
-        pitch = width * static_cast<int>(sizeof(uint32_t));
-    }
-
-    ~Canvas() {
-        SDL_DestroyTexture(frameBuffer);
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-    }
+    Canvas(std::string const& title, int width, int height, int x = SDL_WINDOWPOS_CENTERED, int y = SDL_WINDOWPOS_CENTERED);
+    ~Canvas();
 
     /**
      * @param pixels pixel format RGB888
      */
-    void drawFrame(uint32_t* pixels) {
-        SDL_UpdateTexture(frameBuffer, nullptr, pixels, pitch);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, frameBuffer, nullptr, nullptr);
-        SDL_RenderPresent(renderer);
-    }
-
+    void drawFrame(uint32_t* pixels);
+    bool processInput();
 private:
     SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Texture* frameBuffer;
+    SDL_Event event;
     int pitch;
 };
 
